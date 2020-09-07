0be5c13153147a1e0512b3834bdd484ab64c9d55
Your Name <you@example.com>
2020-09-07 21:06:41 +0800

    add

--- ansible_init.sh
+++ ansible_init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#定义镜像的下载地址
+wget_url="http://d.devdemo.trs.net.cn/hy/devops-hy/"
+
+
 #获取当前的路径
 current_path=`pwd`
 
@@ -19,22 +23,14 @@ images_tmp="tmp-images"
 SOFT_FILE="/TRS/ansible-hy"
 IMAGE_FILE="/TRS/images-hy"
 
-#定义镜像的下载地址
-#wget_url="http://d.devdemo.trs.net.cn/apollo/devops/tools/"
-wget_url="http://d.devdemo.trs.net.cn/hy/devops-hy/"
-
 #定义ansible包后缀
 suffix=".tar.gz"
 
-##定义WeChat、ckm压缩包名
-#wechat_bag="WECHAT.zip"
-#ckm_bag="TRSCKM2017.tar.gz"
-
-#定义docker ansible下载地址
+#定义docker ansible的固定下载地址下载地址
 docker_bag="http://d.devdemo.trs.net.cn/hy/devops-hy/docker-19.03.8.tgz"
 ansible_bag="http://d.devdemo.trs.net.cn/hy/devops-hy/ansible-2.5.1.tar"
 
-
+#-------------------------------开始----------------------------------------------------
 read -p "请输入ansible master主机的IP:"  ansible_ip
 read -p "请输入ansible master主机的ssh端口:"  ansible_port
 read -s -p "请输入ansible master主机的root密码:"  ansible_pass
@@ -67,7 +63,7 @@ if [ $? != 0 ];then
 	echo -e  "\033[33;5mansible主机连接失败，请检查IP、端口、root密码是否正确!\033[0m"
 	exit 2
 fi
-
+rm -rf $current_path/check.txt
 
 
 #创建临时下载目录,保证tmp目录干净
@@ -85,20 +81,14 @@ if [ ! -f $current_path/$images_list  ];then
 fi
 
 #下载docker ansible安装包
-
-#echo -e  "\033[31m镜像本地存放路径:$current_path/$images_tmp\033[0m"
         wget $docker_bag  -P  $current_path/$images_tmp &> /dev/null
         if [ $? != 0 ];then
                 echo -e  "\033[31m$docker_bag下载失败，请手动下载。\033[0m"   
-#        else
-#                echo -e  "\033[32m$docker_bag已经完成下载。\033[0m"       
         fi
 
         wget $ansible_bag  -P  $current_path/$images_tmp &> /dev/null
         if [ $? != 0 ];then
                 echo -e  "\033[31m$ansible_bag下载失败，请手动下载。\033[0m"   
-#        else
-#                echo -e  "\033[32m$ansible_bag已经完成下载。\033[0m"       
         fi
 
 
@@ -107,24 +97,6 @@ fi
 echo -e  "\033[31mansible自动化安装包本地存放路径:$current_path/$file_tmp\033[0m"
 for i in `cat $current_path/$images_list  | grep  -v "#"`	
 do
-if [ $i == "ckm" ];then
-	wget $wget_url$ckm_bag  -P  $current_path/$file_tmp &> /dev/null
-	if [ $? != 0 ];then
-		echo -e  "\033[31m$ckm_bag下载失败，请手动下载。\033[0m"   
-	else 
-		echo -e  "\033[32m$ckm_bag已经完成下载。\033[0m"	
-	fi
- 	continue
-fi
-if [ $i == "wechat" ];then
-	wget $wget_url$wechat_bag  -P  $current_path/$file_tmp &> /dev/null
-	if [ $? != 0 ];then
-		echo -e  "\033[31m$ckm_bag下载失败，请手动下载。\033[0m"   
-	else 
-		echo -e  "\033[32m$wechat_bag已经完成下载。\033[0m"	
-	fi
- 	continue
-fi
 	wget $wget_url$i$suffix -P  $current_path/$file_tmp  &> /dev/null
 	if [ $? != 0 ];then
 		echo -e  "\033[31m$ckm_bag下载失败，请手动下载。\033[0m"   
@@ -134,12 +106,10 @@ fi
 done
 
 #创建ansible master主机ansible包tar.gz存放目录
-ssh -p $ansible_port root@$ansible_ip "mkdir -p $SOFT_FILE"
-ssh -p $ansible_port root@$ansible_ip "mkdir -p $IMAGE_FILE"
+sshpass  -p "$ansible_pass" ssh -p $ansible_port root@$ansible_ip "mkdir -p $SOFT_FILE"
+sshpass  -p "$ansible_pass" ssh -p $ansible_port root@$ansible_ip "mkdir -p $IMAGE_FILE"
 
 #将本地的tar包导入ansible master主机
-
-#echo -e  "\033[31m开始传输镜像到$ansible_ip(ansible)的$IMAGE_FILE目录。\033[0m"
 for i in `ls $current_path/$images_tmp`
 do
 #判断镜像包是否存在
@@ -152,23 +122,16 @@ do
 
         if [ $? != 0 ];then
                 echo -e "\033[31m$i传输失败，请手动下载！\033[0m"
-#        else
-#                echo -e "\033[32m$i传输成功。\033[0m"
 
 fi
 done
 
 
-
-
-
-
-
-
+#传输ansible包
 echo -e  "\033[31m开始传输ansible自动化安装包到$ansible_ip(ansible)的$SOFT_FILE目录。\033[0m"
 for i in `ls $current_path/$file_tmp`
 do
-#判断镜像包是否存在
+#判断包是否存在
 	if [ ! -f  "$current_path/$file_tmp/$i" ];then
 		echo -e "\033[31m$current_path/$file_tmp中没有$i文件，请手动下载！\033[0m"
 		continue
@@ -181,7 +144,9 @@ do
 		continue
 	else 
         	echo -e "\033[32m$i传输成功。\033[0m"
-	ssh -p $ansible_port root@$ansible_ip "cd $SOFT_FILE;tar -zxf $i"
+
+#解压
+sshpass  -p "$ansible_pass"	ssh -p $ansible_port root@$ansible_ip "cd $SOFT_FILE;tar -zxf $i"
 
 fi
 done
@@ -189,8 +154,11 @@ done
 
 #为ansible主机安装ansible工具
 sshpass  -p "$ansible_pass" scp -P $ansible_port  $current_path/$script_ansible_tool   root@$ansible_ip:/tmp  &> /dev/null
-ssh -p $ansible_port root@$ansible_ip "/tmp/$script_ansible_tool  > /root/$script_ansible_tool_result"
+sshpass  -p "$ansible_pass" ssh -p $ansible_port root@$ansible_ip "/tmp/$script_ansible_tool  > /root/$script_ansible_tool_result"
+
+
 #回传安装结果
 sshpass  -p "$ansible_pass" scp -P $ansible_port    root@$ansible_ip:/root/$script_ansible_tool_result  $current_path  &> /dev/null
-echo -e "\033[33m输出$ansible_ip(ansible)主机的ansible安装结果！\033[0m"
+echo -e "\033[33m$ansible_ip(ansible)主机的ansible安装结果！\033[0m"
 cat  $current_path/$script_ansible_tool_result
+rm -rf $current_path/$script_ansible_tool_result
